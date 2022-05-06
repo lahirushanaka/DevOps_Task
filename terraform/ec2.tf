f2e8f82adcd7bc0cee765bd07e75193b80a7e317
Lahiru Sellapperumage <lahiru.sellapperumage@lseg.com>
2022-05-06 17:14:15 +0800

    Subnet issue

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "myinstance" {
   instance_type              = var.instance_type
   iam_instance_profile       = var.iam_instance_profile
   vpc_security_group_ids     = [aws_security_group.app_security_group.id]
-  subnet_id                  =  data.aws_subnets.private.ids[0]
+  subnet_id                  =  data.aws_subnets.subnet.ids[0]
   user_data                  = "${file("userdata.sh")}"
   key_name                   = var.ec2_pem_key
   associate_public_ip_address = var.public_ip
